a62c3696cee6f4c0c90f3ef2ee345155370b1da8
jinaberry2 <jinasproject@gmail.com>
2025-07-15 05:48:35 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@
     const path = window.location.pathname;
 
     // 인증 상태가 아니면 로그인 페이지로 리디렉션
-    if (isAuthenticated !== 'true' && (path.includes('archive.html') || path.includes('write.html') || path.includes('post.html'))) {
+    if (isAuthenticated !== 'true' && (path.includes('archive.html') || path.includes('write.html') || path.includes('post.html') || path.endsWith('/archive'))) {
         window.location.href = 'index.html';
     }
 })();
