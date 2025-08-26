0d4a6294689e86105b79261b2babc9de6805a3d0
jinaberry2 <jinasproject@gmail.com>
2025-08-26 20:09:42 +0900

    Update get-series.js

--- netlify/functions/get-series.js
+++ netlify/functions/get-series.js
@@ -1,1 +1,20 @@
-[]
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async () => {
+  try {
+    const seriesPath = path.join(__dirname, 'series.json');
+    const seriesData = fs.readFileSync(seriesPath, 'utf8');
+    
+    return {
+      statusCode: 200,
+      body: seriesData,
+    };
+  } catch (error) {
+    console.error('Failed to read series data:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Failed to retrieve series data.' }),
+    };
+  }
+};
